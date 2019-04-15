502c05663350681bc04697c87c38ea9926074e20
nostis <norbert_2000@o2.pl>
2019-04-15 13:45:31 +0200

    Fixed one method in crud

--- src/main/java/app/dao/EmployeeDAOCrud.java
+++ src/main/java/app/dao/EmployeeDAOCrud.java
@@ -1,9 +1,16 @@
 package app.dao;
 
 import app.model.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeDAOCrud extends CrudRepository<Employee, Integer>, EmployeeDAOCustom<Employee, Integer> {
+    @Query("select e from Employee e where e.salary = :salary or (e.salary >= (:salary - 500) and e.salary <= (:salary + 500))")
+    @Override
+    List<Employee> findBySalary(@Param("salary")int salary);
 }
--- src/main/java/app/model/Employee.java
+++ src/main/java/app/model/Employee.java
@@ -3,13 +3,11 @@ package app.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
